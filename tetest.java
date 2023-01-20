0802ee7944735ac603510109f986c2e1186bdc84
fwc <2254594630@qq.com>
2023-01-20 09:53:56 +0800

    tetest

---
+++ tetest.java
@@ -0,0 +1,14 @@
+/**
+ * @projectName 031505
+ * @package PACKAGE_NAME
+ * @className tetest
+ * @description this is the tetest class.
+ * @author Fang
+ * @date 2023/1/20 9:52
+ */
+public class tetest {
+    public static void main(String[]args){
+        System.out.println("tetest");
+    }
+
+}
